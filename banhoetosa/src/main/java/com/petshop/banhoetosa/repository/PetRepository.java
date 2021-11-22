39263cbfc5b23ed8e303bdbcc223c4d8a1e90504
RebecaCamilo <rebecacamilo3@gmail.com>
2021-11-22 14:59:14 -0300

    alguma alteração no repository

--- banhoetosa/src/main/java/com/petshop/banhoetosa/repository/PetRepository.java
+++ banhoetosa/src/main/java/com/petshop/banhoetosa/repository/PetRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.petshop.banhoetosa.model.Pet;
 
+//O @Repository tem como objetivo criar beans para a parte de persistência, 
+//além de capturar exceções específicas de persistência e repeti-las novamente 
+//como uma das exceções não verificadas e unificadas do Spring.
 public interface PetRepository extends PagingAndSortingRepository<Pet, Integer> {
 	
 	public Iterable<Pet> findByNameContaining(String partName);
