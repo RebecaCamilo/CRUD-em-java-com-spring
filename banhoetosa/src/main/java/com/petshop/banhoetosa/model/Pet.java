a42d9adbd5089a7b693ba622ce25bd008d67d334
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2021-12-15 10:34:31 -0300

    Update Pet.java
    cometarios sobre a geração de pks em models

--- banhoetosa/src/main/java/com/petshop/banhoetosa/model/Pet.java
+++ banhoetosa/src/main/java/com/petshop/banhoetosa/model/Pet.java
@@ -5,11 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity	//indica que é uma classe de persistencia Java
 public class Pet {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id	//define o atributo abaixo (ou seja, "private int id") como chave primária
+	@GeneratedValue(strategy = GenerationType.AUTO)	//define a estratégia para a geração da pk, "GenerationType.AUTO" ou seja, um um campo auto incrementável nesse caso
 	private int id;
 	private String name;
 	private int age;
